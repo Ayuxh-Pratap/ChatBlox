9a5f9d5c352eab1ee0456b789cf0686cf1774e32
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-03-17 10:04:32 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-3xl mx-auto md:py-10 h-screen'>
+      <div className='h-full border rounded-md'>
+        <div className='h-20'>
+          <div className='p-5 bordeer-b flex items-center justify-between'>
+            <div>
+              <h1 className='text-xl font-bold'>BloxChat.io</h1>
+              <div className='flex items-center gap-1'>
+                <div className='h-4 w-4 bg-green-500 rounded-full animate-pulse'>
+                  
+                </div>
+                <h1 className='text-sm text-gray-400'>9 online</h1>
+              </div>
+            </div>
+            <Button>Login</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
